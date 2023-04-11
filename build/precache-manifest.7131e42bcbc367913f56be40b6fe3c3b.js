37db49a75941a0802a01c2c6bfde99037446cff7
SHRISTI SINGH <ss.shristi.2411@gmail.com>
2023-04-12 03:59:27 +0530

    github build added

---
+++ build/precache-manifest.7131e42bcbc367913f56be40b6fe3c3b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256aa3b00608434a4e838ab37ef8f7d0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "11ef6df4e07dcc58ec19",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "56751a5e672af4199c9c",
+    "url": "/portfolio/static/css/main.511c224c.chunk.css"
+  },
+  {
+    "revision": "11ef6df4e07dcc58ec19",
+    "url": "/portfolio/static/js/2.4d9fe4ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.4d9fe4ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56751a5e672af4199c9c",
+    "url": "/portfolio/static/js/main.cbee6baa.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "ecca46c84baa9108093880c48865b8a0",
+    "url": "/portfolio/static/media/bg-masthead1.ecca46c8.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "773184e334cd743766a32dc04e2a16d0",
+    "url": "/portfolio/static/media/profile4.773184e3.png"
+  }
+]);
